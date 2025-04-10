b80ae3448753ddb41a182abe56e83697c7926838
Md. Mohsin Kabir <mohsinkabirseo@gmail.com>
2025-04-10 20:22:49 +0600

    latest

--- db/db.js
+++ db/db.js
@@ -29,13 +29,13 @@ require('dotenv').config();
 
 
 const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USER}`,
-  `${process.env.DB_PASSWORD}`,
+  // `${process.env.DB_NAME}`,
+  // `${process.env.DB_USER}`,
+  // `${process.env.DB_PASSWORD}`,
 
-  // 'sql12772137', // Replace with your actual database name
-  // 'sql12772137', // Replace with your actual database user
-  // 'uIMTXIAnJR', // Replace with your actual database password
+  'nice_job', // Replace with your actual database name
+  'nice_job', // Replace with your actual database user
+  '886f3pw_V', // Replace with your actual database password
 
   // 'freedb_job_db', // Replace with your actual database name
   // 'freedb_nicesoftware', // Replace with your actual database user
@@ -43,15 +43,15 @@ const sequelize = new Sequelize(
   {
       // host:'82.112.226.90',
       
-    // host: "sql.freedb.tech",
+    host: "localhost",
     // host: "sql12.freesqldatabase.com",
     // host:"mysql-34ea360f-mohsinkabirseo-6632.i.aivencloud.com",
-    host:process.env.DB_HOST,
+    // host:process.env.DB_HOST,
     dialect: "mysql",
     pool: { max: 5, min: 0, idle: 10000 },
     logging: false,
     timezone: "+06:00",
-    port: 3307
+    port: 3306
     // port:25958
   }
 );
